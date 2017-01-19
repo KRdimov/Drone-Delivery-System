f6b02cd36bf9d149d5f7df39845c5cd2bd36459c
Velko <v_kapitanov@yahoo.com>
2017-01-19 02:26:57 +0200

    Update Launher

---
+++ Launcher.java
@@ -0,0 +1,47 @@
+package DroneDeliverySystem;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		runSystem();
+	}
+
+	private static void runSystem() {
+		System.out.println("Your request is:");
+		String request = "4 2016-10-25 12:31 420,369 23 5 54 20"; //scanner.nextLine();
+		System.out.println(request);
+		parseRequest(request);
+	}
+
+	private static void parseRequest(String req) {
+		String[] splitRequest=req.split(",");
+		String[] splitProduct= splitRequest[1].split(" ");
+		int[] extractProduct = new int[splitProduct.length-1]; 
+		
+	   //Parse string array to Integer array
+	    for (int i = 0; i < extractProduct.length; i++) {
+	    	extractProduct[i] = Integer.parseInt(splitProduct[i+1]);
+		}
+	    
+	    //Fill in the HashMap
+	    Map<Integer, Integer> map = new HashMap<>();
+	    for (int i = 0; i < extractProduct.length; i++) {
+	    	//the if statement separate the Even for prodId, the Odd for prod.quantity from the array "extractProduct"
+			if (i%2 == 0) {
+				map.put(extractProduct[i], extractProduct[i+1]);
+			}
+		}
+	    System.out.println();
+	    
+	    //print the HashMap
+	    for (int key : map.keySet()) {
+			System.out.print("Product number:" + key);
+			System.out.println(" Quantity:"+map.get(key));
+		}
+		
+	}
+}
